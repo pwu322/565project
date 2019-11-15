26bbab8854f707d0bf3c66e3b6a9b4d36f129f46
pwu322 <pwu27@uic.edu>
2019-11-15 17:20:40 -0600

    get the input done

--- LS.py
+++ LS.py
@@ -1,7 +1,7 @@
 import re
-input = open('hal.txt')
+input = open('hal.txt')  #import the input file
 
-def get_type(argument):
+def get_type(argument): #input is string, output is type #
     switcher = {
         "ADD": 0,
         "AND": 1,
@@ -18,23 +18,39 @@ def get_type(argument):
     return switcher.get(argument)
 
 
+class node:
+    def __init__(self,id):
+        self.id = id
+        self.child = []
+        self.parent = []
+        self.asap = 0
+        self.alap = 0
+
+ops = []  ## list of nodes
 for line in input:
     newline = line.strip()
     if "label" in newline:
        type = get_type(newline[:3])
        id = int(re.search(r'\d+',newline).group())
-       #print(type) 
-       print(id)
+       ops.append(node(id))
+    
+input.seek(0) #read the file again by reset the read
+
+depend = []
+for line in input:
+    newline = line.strip()
+    if "->" in newline:
+        indexes = list(map(int,re.findall(r'\d+',newline)))
+        depend.append(indexes)
 
 
 
+for i in depend:  ## i is each depency line
+    parent = i[0]   #parent number
+    child = i[1]
+    (ops[parent-1].child).append(ops[child-1])  #parent nodes' child list update
+    (ops[child-1].parent).append(ops[parent-1]) #update nodes' parent list
 
-#class node:
-#    def __init__(self,id):
-"""        self.id = id
-    self.child = []
-    self.parent = []
-    self.asap = 0
-    self.alap = 0
-"""
-
+for op in ops:
+    for j in op.child:
+        print(j.id)
